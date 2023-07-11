3926d9215882318e3a8b00c3bcb69577c5867d55
liuchen11 <liuchen11@asiainfo.com>
2023-07-11 18:45:49 +0800

    geo score test

--- src/test/java/org/lccy/elasticsearch/plugin/MathUtil.java
+++ src/test/java/org/lccy/elasticsearch/plugin/MathUtil.java
@@ -1,5 +1,7 @@
 package org.lccy.elasticsearch.plugin;
 
+import org.elasticsearch.common.geo.GeoPoint;
+import org.elasticsearch.script.ScoreScriptUtils;
 import org.junit.Test;
 import org.lccy.elasticsearch.plugin.function.bo.FieldScoreComputeWapper;
 
@@ -23,5 +25,22 @@ public class MathUtil {
         double data = (1.000000 + -0.200000 * FieldScoreComputeWapper.Modifier.fromString("log1p").apply(600.0)) * 50.000000;
         System.out.printf(data + "");
 
+    }
+
+    @Test
+    public void Test_3() {
+        double data = (0.000000 + 1.000000 * decaygeoexp("31, 33")) * 50.000000;
+        System.out.printf(data + "");
+
+    }
+
+    private double decaygeoexp(String pot) {
+        GeoPoint point = new GeoPoint(pot);
+        String origin = "31,33";
+        String offset = "500m";
+        String scale = "5km";
+        double decay = 0.5;
+        double fieldScore = new ScoreScriptUtils.DecayGeoExp(origin, scale, offset, decay).decayGeoExp(point);
+        return fieldScore;
     }
 }

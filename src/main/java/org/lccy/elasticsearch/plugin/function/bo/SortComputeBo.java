6b4b261389d94ee231d6a04484324a68746a45a7
liuchen11 <liuchen11@asiainfo.com>
2023-07-11 09:06:30 +0800

    function score fixed

--- src/main/java/org/lccy/elasticsearch/plugin/function/bo/SortComputeBo.java
+++ src/main/java/org/lccy/elasticsearch/plugin/function/bo/SortComputeBo.java
@@ -1,12 +1,19 @@
 package org.lccy.elasticsearch.plugin.function.bo;
 
+import org.elasticsearch.common.io.stream.StreamOutput;
+import org.elasticsearch.common.io.stream.Writeable;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Objects;
+
 /**
  * The configuration pojo of sort_socre
  *
  * @author liuchen <br>
  * @date 2023-07-08
  */
-public class SortComputeBo {
+public class SortComputeBo implements Serializable, Writeable {
     private Integer weight;
     private String field;
     private String type;
@@ -48,9 +55,31 @@ public class SortComputeBo {
         return this;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SortComputeBo that = (SortComputeBo) o;
+        return weight.equals(that.weight) && field.equals(that.field) && Objects.equals(type, that.type) && value.equals(that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(weight, field, type, value);
+    }
+
+    @Override
+    public void writeTo(StreamOutput out) throws IOException {
+        out.writeString(field);
+        out.writeInt(weight);
+        out.writeOptionalString(type);
+        out.writeOptionalString(value);
+    }
+
+
     public boolean match(String[] values) {
-        for(String val : values) {
-            if(this.value.equals(val)) {
+        for (String val : values) {
+            if (this.value.equals(val)) {
                 return !Type.NOT.equals(this.type);
             }
         }
